57c1c0ad313d7d181e3250bf8b3ea085ea6307be
Sahand Zarrinkoub <s.zarrinkoub@reply.de>
2022-04-08 10:25:01 +0200

    create avro to produce

--- src/main/scala/com/datareply/multitenant/Producer.scala
+++ src/main/scala/com/datareply/multitenant/Producer.scala
@@ -17,14 +17,27 @@ object Producer {
 
   def produceToKafka(file: File): Unit = {
     val bufferedSource: BufferedSource = io.Source.fromFile(file)
-    bufferedSource.getLines().foreach(line => {
-      Thread.sleep(1)
-      produceToKafka(line)
+    bufferedSource.getLines()
+      .map(_.split(";"))
+      .filter((values: Array[String]) => "BME280".equals(values(1)))
+      .foreach(values => {
+        Thread.sleep(1)
+        produceToKafka(values)
     })
   }
 
-  def produceToKafka(line: String): Unit = {
-    val values: Array[String] = line.split(";") // TODO: Extract config
-
+  def produceToKafka(values: Array[String]): Unit = {
+    Measurement.newBuilder()
+      .setSensorId(values(0))
+      .setSensorType(values(1))
+      .setLocation(values(2).toInt)
+      .setLat(values(3).toLong)
+      .setLon(values(4).toLong)
+      .setTimestamp(values(5))
+      .setPressure(values(6).toLong)
+      .setAltitude(values(7).toLong)
+      .setPressureSealevel(values(8).toLong)
+      .setTemperature(values(9).toLong)
+      .setHumidity(values(10).toLong)
   }
 }
